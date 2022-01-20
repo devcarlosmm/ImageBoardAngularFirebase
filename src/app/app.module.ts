ac04a4520643d9daa7dba0ffda971dd22178a650
Carlos Morillo Moreno <c.morillo.moreno@gmail.com>
2022-01-20 12:11:16 +0100

    creacion de categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MenubarModule } from 'primeng/menubar';
+import { CategoryPageComponent } from './categories/pages/category-page/category-page.component';
+import { FormPostComponent } from './categories/components/form-post/form-post.component';
+import { DetailPostComponent } from './categories/components/detail-post/detail-post.component';
+import { ReplyPostComponent } from './categories/components/reply-post/reply-post.component';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, CategoryPageComponent, FormPostComponent, DetailPostComponent, ReplyPostComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
---
+++ src/app/categories/components/reply-post/reply-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-post',
+  templateUrl: './reply-post.component.html',
+  styleUrls: ['./reply-post.component.scss']
+})
+export class ReplyPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
