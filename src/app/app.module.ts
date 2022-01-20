9a99889f00a6168c8fe94376f3112c14e189c78c
Carlos Morillo Moreno <c.morillo.moreno@gmail.com>
2022-01-20 13:39:53 +0100

    404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { MenubarModule } from 'primeng/menubar';
 import { CategoryPageComponent } from './categories/pages/category-page/category-page.component';
 import { FormPostComponent } from './categories/components/form-post/form-post.component';
 import { DetailPostComponent } from './categories/components/detail-post/detail-post.component';
 import { ReplyPostComponent } from './categories/components/reply-post/reply-post.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+
+//PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, CategoryPageComponent, FormPostComponent, DetailPostComponent, ReplyPostComponent, HomeComponent, NotfoundComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoryPageComponent,
+    FormPostComponent,
+    DetailPostComponent,
+    ReplyPostComponent,
+    HomeComponent,
+    NotfoundComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MenubarModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
