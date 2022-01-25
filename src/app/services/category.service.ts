c7b4fd1d36b1148826f252d2a2340f7a7cb4404c
Yuumei <yuumei3d@gmail.com>
2022-01-25 12:57:17 +0100

    service init

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Firestore, doc, onSnapshot, DocumentReference, docSnapshots } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+}
