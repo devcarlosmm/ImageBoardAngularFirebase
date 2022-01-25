6eec5ada4f0c8df757a3cb17d1539bc65f27f0d4
Carlos Morillo Moreno <c.morillo.moreno@gmail.com>
2022-01-25 12:41:19 +0100

    interfaces firebase

---
+++ src/app/interfaces/post.interface.ts
@@ -0,0 +1,7 @@
+export interface Post {
+  id: string;
+  category: string;
+  content: string;
+  img: string;
+  title: string;
+}
---
+++ src/app/interfaces/reply.interface.ts
@@ -0,0 +1,7 @@
+export interface Reply {
+  idReply: string;
+  idPost: string;
+  content: string;
+  img?: string;
+  entries: string[];
+}
