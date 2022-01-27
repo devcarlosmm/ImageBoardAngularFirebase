1ffd1e267c1c559ac2893f5e45a02a2dc8338d91
Yuumei <yuumei3d@gmail.com>
2022-01-27 13:09:13 +0100

    unbelievable magical card image

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { CardModule } from 'primeng/card';
+
+import { CategoryPageComponent } from './pages/category-page/category-page.component';
+import { ReplyPostComponent } from './components/reply-post/reply-post.component';
+import { DetailPostComponent } from './components/detail-post/detail-post.component';
+import { FormPostComponent } from './components/form-post/form-post.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CategoriesRoutingModule],
+  declarations: [
+    CategoryPageComponent,
+    ReplyPostComponent,
+    DetailPostComponent,
+    FormPostComponent
+  ],
+  imports: [
+    CommonModule, 
+    CategoriesRoutingModule,
+    CardModule
+  ],
 })
 export class CategoriesModule {}
