dfe6b326375bb9535ad4146d000e7bac8863673b
Yuumei <yuumei3d@gmail.com>
2022-01-27 13:22:07 +0100

    fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { CategoryPageComponent } from './categories/pages/category-page/category-page.component';
-import { FormPostComponent } from './categories/components/form-post/form-post.component';
-import { DetailPostComponent } from './categories/components/detail-post/detail-post.component';
-import { ReplyPostComponent } from './categories/components/reply-post/reply-post.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 
