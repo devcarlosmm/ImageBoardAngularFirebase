dfe638fc23eefbfc6fcbec6f151e17768083f736
Yuumei <yuumei3d@gmail.com>
2022-01-28 13:50:28 +0100

    category changes

--- src/app/categories/pages/category-page/category-page.component.ts
+++ src/app/categories/pages/category-page/category-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CategoryService } from 'src/app/services/category.service';
 import { Post } from '../../../interfaces/post.interface';
 
@@ -7,14 +9,38 @@ import { Post } from '../../../interfaces/post.interface';
   templateUrl: './category-page.component.html',
   styleUrls: ['./category-page.component.scss'],
 })
-export class CategoryPageComponent implements OnInit {
+export class CategoryPageComponent implements OnInit{
   posts: Post[] = [];
+  category:string = "";
+  currentCategory:string = "";
+  subscription:Subscription;
   
-  constructor(categoryService: CategoryService) {
-    categoryService.getPosts().then((data) => {
+  constructor(private categoryService: CategoryService, private router:Router) {
+    this.subscription = this.router.events.subscribe({
+      next: (event) => {
+        if(event instanceof NavigationEnd){
+          if(event.url.split("/")[2] != this.currentCategory){
+            this.retrievePosts();
+          }
+        }
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    window.onbeforeunload = () => this.unsubscribeOnChange();
+  }
+
+  retrievePosts():void{
+    this.posts = [];
+    this.category = this.router.url.split("/")[2];
+    this.currentCategory = this.category;
+    this.categoryService.getPosts(this.category).then((data) => {
       this.posts = data;
     });
   }
 
-  ngOnInit(): void {}
+  unsubscribeOnChange():void{
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
 
 //FIREBASE
-import { collection, Firestore, getDocs, getFirestore } from '@angular/fire/firestore';
+import { collection, Firestore, getDocs, getFirestore, query, where } from '@angular/fire/firestore';
 import { initializeApp } from "@angular/fire/app";
 import { Post } from '../interfaces/post.interface';
 
@@ -11,17 +11,16 @@ import { Post } from '../interfaces/post.interface';
 })
 export class CategoryService {
 
-  constructor(private fs:Firestore) {
-    
-  }
+  constructor(private fs:Firestore) {}
 
-  async getPosts():Promise<Post[]>{
+  async getPosts(category:string):Promise<Post[]>{
     const firebaseApp = initializeApp(environment.firebase);
     const db = getFirestore(firebaseApp);
 
     const postsCollection = collection(db, "post");
-    const postsSnapshot = await getDocs(postsCollection);
-    const postsList = postsSnapshot.docs.map(doc => doc.data());
+    const q = query(postsCollection, where("category", "==", category))
+    const querySnapshot = await getDocs(q);
+    const postsList = querySnapshot.docs.map(doc => doc.data());
 
     return postsList as Post[];
   }
