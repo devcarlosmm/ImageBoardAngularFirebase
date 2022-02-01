5e6241d6328086d2d650e35f358e8b36dc40f317
Carlos Morillo Moreno <c.morillo.moreno@gmail.com>
2022-02-01 12:26:55 +0100

    Cambios en category, reply y details

--- src/app/categories/components/detail-post/detail-post.component.ts
+++ src/app/categories/components/detail-post/detail-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post.interface';
+import { CategoryService } from '../../../services/category.service';
 
 @Component({
   selector: 'app-detail-post',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-post.component.scss'],
 })
 export class DetailPostComponent implements OnInit {
-  constructor() {}
+  detailPost: Post = {
+    id: '',
+    category: '',
+    content: '',
+    img: '',
+    title: '',
+  };
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {}
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,27 +1,45 @@
 import { Injectable } from '@angular/core';
-import { environment } from "../../environments/environment";
+import { environment } from '../../environments/environment';
 
 //FIREBASE
-import { collection, Firestore, getDocs, getFirestore, query, where } from '@angular/fire/firestore';
-import { initializeApp } from "@angular/fire/app";
+import {
+  collection,
+  Firestore,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from '@angular/fire/firestore';
+import { initializeApp } from '@angular/fire/app';
 import { Post } from '../interfaces/post.interface';
+import { doc, getDoc } from 'firebase/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  firebaseApp = initializeApp(environment.firebase);
+  db = getFirestore(this.firebaseApp);
+  constructor(private fs: Firestore) {}
 
-  constructor(private fs:Firestore) {}
-
-  async getPosts(category:string):Promise<Post[]>{
-    const firebaseApp = initializeApp(environment.firebase);
-    const db = getFirestore(firebaseApp);
-
-    const postsCollection = collection(db, "post");
-    const q = query(postsCollection, where("category", "==", category))
+  async getPosts(category: string): Promise<Post[]> {
+    const postsCollection = collection(this.db, 'post');
+    const q = query(postsCollection, where('category', '==', category));
     const querySnapshot = await getDocs(q);
-    const postsList = querySnapshot.docs.map(doc => doc.data());
+    const postsList = querySnapshot.docs.map((doc) => doc.data());
 
     return postsList as Post[];
+  }
+
+  async getDetailPost(pIdPost: any) {
+    const docRef = doc(this.db, 'post', pIdPost);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log('Document data:', docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log('No such document!');
+    }
   }
 }
